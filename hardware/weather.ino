4107d2e1cdfd6da77baa6daa81c24be33d9bf562
Mathijs <thiezn@gmail.com>
2026-07-23 15:39:17 +0200

    Fix hardware resource path

--- hardware/weather.ino
+++ hardware/weather.ino
@@ -7,9 +7,9 @@
 int status = WL_IDLE_STATUS; // WiFi radio status
 
 // --- Server Configuration ---
-const char server[] = "pap.mortimer.nl"; // Target domain (DNS will resolve this)
-const char resource[] = "/iot/weather";  // API endpoint path
-const int port = 443;                    // Public HTTPS port
+const char server[] = "pap.mortimer.nl";   // Target domain (DNS will resolve this)
+const char resource[] = "/api/v1/weather"; // API endpoint path
+const int port = 443;                      // Public HTTPS port
 
 // Instantiate the secure WiFi client
 WiFiSSLClient sslClient;
